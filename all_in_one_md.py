b8f84e81744f0465f31bfbdfc4f998c6f6217028
JerryQ <1397200108@qq.com>
2024-01-24 21:34:22 +0800

    主要功能完成

--- all_in_one_md.py
+++ all_in_one_md.py
@@ -1,36 +1,76 @@
-from typing import Union
-from pathlib import Path
 import codecs
-import chardet
-from markdown2pdf import convert
+import warnings
+from pathlib import Path
 
-def merge_md(root: Path = Path('C:\\Users\\114514\\Desktop\\print\\gitbook_name'),
-             dest_file: Path = Path('C:\\Users\\114514\\Desktop\\print\\gitbook_name\\.gitbook\\all_in_one.md'),
-             dest_encoding: str = 'utf8',
-             recurse: bool = True,
-             ):
-    
-    root, dest_file = Path(root), Path(dest_file)
+import pdfkit
+from markdown import markdown
 
-    if not root.is_dir():
-        return
 
-    pattern = '**/*.md' if recurse else '*.md'
+class GitBook:
+    __slots__ = (
+        "working_directory",
+        "output_filepath",
+        "tmp_md_filepath",
+        "encoding",
+        "recursive",
+    )
 
-    with open(dest_file, 'w', encoding=dest_encoding) as dest:
-        for txt in Path(root).glob(pattern):
-            if txt.absolute() == dest_file.absolute():
-                continue
+    def __init__(
+        self,
+        working_directory: str,
+        output_filepath: str,
+        encoding: str,
+        recursive: bool,
+    ):
+        """见main.py中的argparse部分。"""
+        self.working_directory = Path(working_directory)
+        if not self.working_directory.is_dir():
+            raise ValueError(f"{self.working_directory} is not a directory.")
+        self.output_filepath = Path(output_filepath)
+        if self.output_filepath.exists():
+            warnings.warn("The output file already exists and will be overwritten.")
+        self.tmp_md_filepath = self.output_filepath.with_suffix(".md").absolute()
+        if self.tmp_md_filepath.exists():
+            warnings.warn(
+                "The temporary markdown file already exists and will be overwritten."
+            )
+        self.encoding = codecs.lookup(encoding).name
+        self.recursive = recursive
 
-            print(txt.__str__() + "\n")
+    def _find_markdowns(self):
+        pattern = "**/*.md" if self.recursive else "*.md"
 
-            text = txt.read_text(encoding='utf8')
-            strContent = text.replace("# ", "## ")
-            strContent = strContent.replace("## 第", "# 第")
-            print(strContent)
-            dest.write(strContent)
+        def not_opt(fp: Path):
+            return fp.absolute() != self.tmp_md_filepath  # tmp_md_filepath已保证为绝对路径
 
-    convert(dest_file, pdf_file)
+        for f in filter(not_opt, self.working_directory.glob(pattern)):
+            yield (
+                f.read_text(encoding=self.encoding)
+                .replace("# ", "## ")
+                .replace("## 第", "# 第")
+            )
 
-if __name__ == '__main__':
-    merge_md() 
+    def _convert(self, html: str):
+        pdfkit.from_string(
+            markdown(html, extensions=["extra", "codehilite"], output_format="html"),
+            self.output_filepath,
+            configuration=pdfkit.configuration(
+                wkhtmltopdf="wkhtmltopdf/wkhtmltopdf.exe"
+            ),
+            options={"encoding": self.encoding},
+        )
+
+    def merge(self):
+        """将指定工作路径下的md文件合并成一个md文件并写入到指定的输出文件中。"""
+        with open(self.tmp_md_filepath, "w", encoding=self.encoding) as f:
+            f.writelines(self._find_markdowns())
+
+    def convert(self):
+        """将指定的md文件转换成pdf文件。"""
+        with open(self.tmp_md_filepath, encoding=self.encoding) as f:
+            html = f.read()
+        self._convert(html)
+
+    def merge_and_convert(self):
+        """将指定工作路径下的md文件合并成一个md文件并转换成pdf文件。"""
+        self._convert("\n".join(self._find_markdowns()))
